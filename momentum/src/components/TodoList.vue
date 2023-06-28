424850a31bb58942271377595882fe4c3510687c
geol2 <big9401@gmail.com>
2023-06-28 14:00:21 +0900

    [Vue] 할일 목록 추가

--- momentum/src/components/TodoList.vue
+++ momentum/src/components/TodoList.vue
@@ -1,12 +1,31 @@
 <template>
-    <div id="app">
-        list
-    </div>
+        <section>
+            <ul>
+                <li v-for="todoItem in todoItems" v-bind:key="todoItem">
+                    <i class="checkBtn fas fa-check" aria-hidden="true"></i>
+                    {{ todoItem }}
+                    <span class="removeBtn" type="button" @click="removeTodo(todoItem, index)">
+                        <i class="far fa-trash-alt" aria-hidden="true"></i>
+                    </span>
+                </li>
+            </ul>
+        </section>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            todoItems: []
+        }
+    },
+    created() {
+        if(localStorage.length > 0) {
+            for(let i = 0; i < localStorage.length; i++) {
+                this.todoItems.push( localStorage.key(i) );
+            }
+        }
+    }
 }
 </script>
 
